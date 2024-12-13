c1c15107807997620e799b95f8d7a6bec08b94ab
bagassambega <bagassambega@gmail.com>
2024-12-13 20:18:19 +0700

    fix: change datatype to mac friendly

--- src/NaiveBayes.py
+++ src/NaiveBayes.py
@@ -1,8 +1,11 @@
+import pickle
 import pandas as pd
 from typing import List, Dict
 
 class NaiveBayes:
-    def __init__(self, data_train: pd.DataFrame, categorical_columns: List[str], numerical_columns: List[str], target_column: str):
+    def __init__(self, data_train: pd.DataFrame, categorical_columns: List[str], numerical_columns: List[str], target_column: str,
+                 data_train_freq: Dict[str, Dict[str, Dict[str, int]]] = {}, data_train_prob: Dict[str, Dict[str, Dict[str, float]]] = {},
+                    target_freq: Dict[str, int] = {}, target_prob: Dict[str, float] = {}, binning_info: Dict[str, pd.IntervalIndex] = {}):
         """
         :param data_train: data used to train the model
         :param categorical_columns: list of column name that contains categorical values
@@ -12,12 +15,12 @@ class NaiveBayes:
         self.data_train: pd.DataFrame = data_train
         self.categorical_columns: List[str] = categorical_columns
         self.numerical_columns: List[str] = numerical_columns
-        self.data_train_freq: Dict[str, Dict[str, Dict[str, int]]] = {} # [key: column name, value: [key: target unique values, value: [key: unique value of column, value: frequency of unique value for each column]]]
-        self.data_train_prob: Dict[str, Dict[str, Dict[str, float]]] = {} # [key: column name, value: [key: target unique values, value: [key: unique value of column, value: probability of unique value for each column]]]
+        self.data_train_freq: Dict[str, Dict[str, Dict[str, int]]] = data_train_freq # [key: column name, value: [key: target unique values, value: [key: unique value of column, value: frequency of unique value for each column]]]
+        self.data_train_prob: Dict[str, Dict[str, Dict[str, float]]] = data_train_prob # [key: column name, value: [key: target unique values, value: [key: unique value of column, value: probability of unique value for each column]]]
         self.target_column: str = target_column
-        self.target_freq: Dict[str, int] = {} # [key: target unique values, value: frequency of target unique values]
-        self.target_prob: Dict[str, float] = {} # [key: target unique values, value: probability of target unique values]
-
+        self.target_freq: Dict[str, int] = target_freq # [key: target unique values, value: frequency of target unique values]
+        self.target_prob: Dict[str, float] = target_prob # [key: target unique values, value: probability of target unique values]
+        self.binning_info: Dict[str, pd.IntervalIndex] = binning_info # [key: column name, value: binning information]
 
     def calculate_frequency_column(self):
         for col in self.categorical_columns:
@@ -47,17 +50,17 @@ class NaiveBayes:
         print(self.target_freq)
 
 
-    """
-    Use quantile to create bins for numerical column
-    """
     def create_bins_numerical_column(self, col: str, n_bins: int):
+        """
+        Use quantile to create bins for numerical column
+        """
         return pd.qcut(self.data_train[col], n_bins, duplicates='drop')
 
 
-    """
-    Calculate probability based on frequency of each column, relative to the target column
-    """
     def calculate_probability_column(self):
+        """
+        Calculate probability based on frequency of each column, relative to the target column
+        """
         for col in self.categorical_columns:
             self.data_train_prob[col] = {}
             for target in self.data_train[self.target_column].unique():
@@ -77,19 +80,21 @@ class NaiveBayes:
         for target in self.data_train[self.target_column].unique():
             self.target_prob[target] = self.target_freq[target] / self.data_train.shape[0]
 
-    """
-    Train the model
-    """
     def train(self):
+        """
+        Train the model
+        """
         self.calculate_frequency_column()
         self.calculate_probability_column()
 
 
-    """
-    Predict the data. Input is just one row
-    """
     def predict(self, data_test: pd.DataFrame):
+        """
+        Predict the data. Input is just one row
+        """
         result = {}
+        for target in self.data_train[self.target_column].unique():
+            print(target)
         for target in self.data_train[self.target_column].unique():
             prob = self.target_prob[target]
             for col in self.categorical_columns:
@@ -99,15 +104,55 @@ class NaiveBayes:
             result[target] = prob
         return max(result, key=result.get)
 
+    def save_model(self, model_file_name: str = 'naive_bayes_model.pkl'):
+        """
+        Save the entire model to a pickle file.
 
-    """
-    Save the model to a txt file
-    """
-    def save_model(self, model_file_name: str = 'naive_bayes_model.txt'):
-        file_name: str = model_file_name
-        with open(file_name, 'w') as file:
-            file.write(str(self.data_train_prob))
-            file.write(str(self.target_prob))
+        :param model_file_name: Name of the file to save the model
+        """
+        with open(model_file_name, 'wb') as file:
+            pickle.dump({
+                'data_train': self.data_train,
+                'data_train_freq': self.data_train_freq,
+                'data_train_prob': self.data_train_prob,
+                'target_freq': self.target_freq,
+                'target_prob': self.target_prob,
+                'categorical_columns': self.categorical_columns,
+                'numerical_columns': self.numerical_columns,
+                'target_column': self.target_column,
+                'binning_info': self.binning_info
+            }, file)
+        print(f"Model saved to {model_file_name}")
+
+    @classmethod
+    def load_model(cls, model_file_name: str = 'naive_bayes_model.pkl'):
+        """
+        Load a previously saved model.
+
+        :param model_file_name: Name of the file to load the model from
+        :return: Loaded NaiveBayes model
+        """
+        with open(model_file_name, 'rb') as file:
+            model_data = pickle.load(file)
+
+        # Create a dummy model instance
+        model = cls(pd.DataFrame(),
+                    model_data['categorical_columns'],
+                    model_data['numerical_columns'],
+                    model_data['target_column'])
+
+        # Restore model attributes
+        model.data_train_freq = model_data['data_train_freq']
+        model.data_train_prob = model_data['data_train_prob']
+        model.target_freq = model_data['target_freq']
+        model.data_train = model_data['data_train']
+        model.target_prob = model_data['target_prob']
+        model.target_column = model_data['target_column']
+        model.binning_info = model_data['binning_info']
+
+        # print(model.data_train)
+
+        return model
 
 
 if __name__ == "__main__":
@@ -127,5 +172,5 @@ if __name__ == "__main__":
     # for col, value in nb.data_train_freq.items():
     #     for target, freq in value.items():
     #         print(col, target)
-    print(df.iloc[96])
-    print(nb.predict(df.iloc[96]))
+    print(df.iloc[107])
+    print(nb.predict(df.iloc[107]))
